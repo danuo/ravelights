03f2866379d0ac957d43c2dc08e183564a389471
Daniel <d.brummerloh@gmail.com>
2023-08-03 21:12:07 +0200

    WIP add modes to EffectColorize

--- src/ravelights/effects/effect_colorize.py
+++ src/ravelights/effects/effect_colorize.py
@@ -1,9 +1,10 @@
+import random
+
 import numpy as np
 
 from ravelights.core.colorhandler import Color
 from ravelights.core.custom_typing import Array
 from ravelights.effects.effect_super import Effect
-from ravelights.vfilters.vfilter_bw import VfilterBW
 
 
 class EffectColorize(Effect):
@@ -12,23 +13,37 @@ class EffectColorize(Effect):
         hue_range controls the color variation for each frame
         """
 
-        self.bw_filter = VfilterBW(root=self.root, device=self.device)  # do not use this
+        self.color_matrix = np.zeros(shape=(self.n_leds, self.n_lights), dtype=int)
+
+        self.mode = random.choice(["up_down", "blocks"])
+        self.mode = "blocks"
+        match self.mode:
+            case "up_down":
+                # demo color segmentation
+                self.color_matrix[:70, :] = 1
+            case "blocks":
+                i = 0
+                sign = False
+                # self.color_matrix = self.color_matrix.flatten()
+                print(self.color_matrix.shape)
+                while i < self.n:
+                    number = random.randint(5, 50)
+                    self.color_matrix[i : i + number] = sign
+                    sign = not sign
+                    i += number
+                # self.color_matrix = self.color_matrix.reshape((self.n_leds, self.n_lights))
 
     def render_settings_overwrite(self, selected_level: int) -> dict[str, Color]:
         return dict()
 
     def render_matrix(self, in_matrix: Array, color: Color) -> Array:
-        # demo color segmentation
-        color_matrix = np.zeros(in_matrix.shape[:2])
-        color_matrix[:70, :] = 1
-
         # bw filter
         in_matrix_bw = np.max(in_matrix, axis=-1)
 
         color_1, color_2, color_effect = self.settings.color_engine.get_colors_rgb(selected_level=1)
         in_matrix_color1 = self.colorize_matrix(in_matrix_bw, color=color_1)
         in_matrix_color2 = self.colorize_matrix(in_matrix_bw, color=color_2)
-        out_matrix_rgb = np.where(color_matrix[..., None].repeat(3, axis=2) == 1, in_matrix_color1, in_matrix_color1)
+        out_matrix_rgb = np.where(self.color_matrix[..., None].repeat(3, axis=2) == 1, in_matrix_color1, in_matrix_color2)
         return out_matrix_rgb
 
     def on_delete(self):
