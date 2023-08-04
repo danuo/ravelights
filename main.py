e62cc9b3b19d545f66ea7b69ccdfdbe16abef9b4
Daniel <d.brummerloh@gmail.com>
2023-08-05 00:29:15 +0200

    remove if __name__ == "__main__":, file won't be imported

--- main.py
+++ main.py
@@ -33,60 +33,59 @@ def parse_args():
     return args
 
 
-if __name__ == "__main__":
-    args = parse_args()
+args = parse_args()
 
-    # ------------------------------ port selection ------------------------------ #
+# ------------------------------ port selection ------------------------------ #
 
-    """
-    Case A (default)
-    --webui
-    web static via flask @ port 80
-    rest via flask @ port 80
+"""
+Case A (default)
+--webui
+web static via flask @ port 80
+rest via flask @ port 80
 
-    Case B
-    --no-webui
-    web dynamic via quasar dev @ port 80
-    rest via flask @ port 5000
+Case B
+--no-webui
+web dynamic via quasar dev @ port 80
+rest via flask @ port 5000
 
-    Case C
-    --no-webui
-    web static via nginx @ port 80
-    rest via flask @ port 5000
-    """
+Case C
+--no-webui
+web static via nginx @ port 80
+rest via flask @ port 5000
+"""
 
-    webserver_port = 80
-    if not args.webui:
-        webserver_port = 5000
-        logger.info("Running flask on port 5000, such that the web interface can be served by quasar or nginx on port 80")
+webserver_port = 80
+if not args.webui:
+    webserver_port = 5000
+    logger.info("Running flask on port 5000, such that the web interface can be served by quasar or nginx on port 80")
 
-    artnet = None
-    if args.artnet_wifi:
-        from ravelights.artnet.artnet_udp_transmitter import \
-            ArtnetUdpTransmitter  # fmt: skip
-        artnet = ArtnetUdpTransmitter(ip_address=args.artnet_address)
-    elif args.artnet_serial:
-        from ravelights.artnet.artnet_serial_transmitter import \
-            ArtnetSerialTransmitter  # fmt: skip
-        artnet = ArtnetSerialTransmitter(serial_port_address=args.artnet_serial_port, baud_rate=args.artnet_serial_baudrate)
+artnet = None
+if args.artnet_wifi:
+    from ravelights.artnet.artnet_udp_transmitter import \
+        ArtnetUdpTransmitter  # fmt: skip
+    artnet = ArtnetUdpTransmitter(ip_address=args.artnet_address)
+elif args.artnet_serial:
+    from ravelights.artnet.artnet_serial_transmitter import \
+        ArtnetSerialTransmitter  # fmt: skip
+    artnet = ArtnetSerialTransmitter(serial_port_address=args.artnet_serial_port, baud_rate=args.artnet_serial_baudrate)
 
-    app = RaveLightsApp(
-        fps=args.fps,
-        artnet_transmitter=artnet,
-        webserver_port=webserver_port,
-        visualizer=args.visualizer,
-        serve_webinterface=args.webui,
-    )
+app = RaveLightsApp(
+    fps=args.fps,
+    artnet_transmitter=artnet,
+    webserver_port=webserver_port,
+    visualizer=args.visualizer,
+    serve_webinterface=args.webui,
+)
 
-    if not args.profiling:
-        app.run()
-    else:
-        logging.info("To visualize the profiling file, run `snakeviz ./log/profiling.prof` in terminal")
-        with cProfile.Profile() as pr:
-            app.profile()
+if not args.profiling:
+    app.run()
+else:
+    logging.info("To visualize the profiling file, run `snakeviz ./log/profiling.prof` in terminal")
+    with cProfile.Profile() as pr:
+        app.profile()
 
-        stats = pstats.Stats(pr)
-        stats.sort_stats(pstats.SortKey.TIME)
-        # stats.print_stats()
-        filename = Path("log") / "profiling.prof"
-        stats.dump_stats(filename=filename)
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    # stats.print_stats()
+    filename = Path("log") / "profiling.prof"
+    stats.dump_stats(filename=filename)
