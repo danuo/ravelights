f2af5a06335f8e2ba7c98b61c517b5ca2bc4f204
Daniel <d.brummerloh@gmail.com>
2023-08-14 00:34:32 +0200

    add more light layouts, switch default layout to 5/10 lights

--- src/ravelights/configs/visualizer_configurations.py
+++ src/ravelights/configs/visualizer_configurations.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-X_SPACINGS_1 = np.linspace(0.2, 0.8, 5)
+X_SPACINGS = dict()
+for i in range(1, 11):
+    X_SPACINGS[i] = np.linspace(0.2, 0.8, i)
+
 configurations = [
     # ─── CONFIGURATION 1 ─────────────────────────────────────────────────
     # this is the default configuration with 2 devices n_leds=144 and
@@ -15,20 +18,75 @@ configurations = [
         visualizer_config=[
             # ─── DEVICE 1 ────────────────────────────────────────────────────
             [
-                dict(x=X_SPACINGS_1[0], y=0.3, rot=0.0, scale=1.0),
-                dict(x=X_SPACINGS_1[1], y=0.3, rot=0.0, scale=1.0),
-                dict(x=X_SPACINGS_1[2], y=0.3, rot=0.0, scale=1.0),
-                dict(x=X_SPACINGS_1[3], y=0.3, rot=0.0, scale=1.0),
-                dict(x=X_SPACINGS_1[4], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][0], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][1], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][2], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][3], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][4], y=0.3, rot=0.0, scale=1.0),
             ],
             # ─── DEVICE 2 ────────────────────────────────────────────────────
             [
-                dict(x=X_SPACINGS_1[0], y=0.7, rot=0.0, scale=1.0),
-                dict(x=X_SPACINGS_1[1], y=0.7, rot=0.0, scale=1.0),
-                dict(x=X_SPACINGS_1[2], y=0.7, rot=0.0, scale=1.0),
-                dict(x=X_SPACINGS_1[3], y=0.7, rot=0.0, scale=1.0),
-                dict(x=X_SPACINGS_1[4], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][0], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][1], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][2], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][3], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][4], y=0.7, rot=0.0, scale=1.0),
             ],
         ],
-    )
+    ),
+    dict(
+        name="5 top, 10 bottom",
+        device_config=[dict(n_lights=5, n_leds=144), dict(n_lights=10, n_leds=144)],
+        visualizer_config=[
+            # ─── DEVICE 1 ────────────────────────────────────────────────────
+            [
+                dict(x=X_SPACINGS[5][0], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][1], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][2], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][3], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[5][4], y=0.3, rot=0.0, scale=1.0),
+            ],
+            # ─── DEVICE 2 ────────────────────────────────────────────────────
+            [
+                dict(x=X_SPACINGS[10][0], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][1], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][2], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][3], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][4], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][5], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][6], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][7], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][8], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][9], y=0.7, rot=0.0, scale=1.0),
+            ],
+        ],
+    ),
+    dict(
+        name="6 top, 10 bottom",
+        device_config=[dict(n_lights=6, n_leds=144), dict(n_lights=10, n_leds=144)],
+        visualizer_config=[
+            # ─── DEVICE 1 ────────────────────────────────────────────────────
+            [
+                dict(x=X_SPACINGS[6][0], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[6][1], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[6][2], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[6][3], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[6][4], y=0.3, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[6][5], y=0.3, rot=0.0, scale=1.0),
+            ],
+            # ─── DEVICE 2 ────────────────────────────────────────────────────
+            [
+                dict(x=X_SPACINGS[10][0], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][1], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][2], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][3], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][4], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][5], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][6], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][7], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][8], y=0.7, rot=0.0, scale=1.0),
+                dict(x=X_SPACINGS[10][9], y=0.7, rot=0.0, scale=1.0),
+            ],
+        ],
+    ),
 ]
