9ce6826450c3709acdda39085bb7bf19bc3ba59e
Daniel <d.brummerloh@gmail.com>
2023-08-17 18:04:30 +0200

    PatternMeteor: fix bugs, update sync method

--- src/ravelights/patterns/pattern_meteor.py
+++ src/ravelights/patterns/pattern_meteor.py
@@ -14,7 +14,6 @@ class PatternMeteor(Pattern):
     def init(self):
         self.p_add_dimmer = 0.0
         self.matrix = self.get_float_matrix_2d_mono()
-        self.kwargs = dict()
         self.load_version()
 
     def load_version(self):
@@ -68,40 +67,38 @@ class PatternMeteor(Pattern):
         # this function will set all parameters randomly. called after init_custom()
         # ─── LIGHT ALTERNATION ───────────────────────────────────────────
         if 1 in modes:
-            light_selection = random.choice(["all", "half", "random"])
-            self.kwargs["light_selection"] = light_selection
+            self.light_selection = random.choice(["all", "half", "random"])
         # ─── FLIP ALTERNATION ────────────────────────────────────────────
         if 2 in modes:
             flip = True if p(0.5) else False
-            self.kwargs["flip"] = flip
+            self.flip = flip
         if 3 in modes:
             mirror = random.choices(
                 population=["none", "out_to_in", "in_to_out", "through", "through_magic"],
                 weights=[6.0, 1.0, 1.0, 1.0, 1.0],
             )[0]
-            self.kwargs["mirror"] = mirror
+            self.mirror = mirror
         if 4 in modes:
             flicker = True if p(0.05) else False
-            self.kwargs["flicker"] = flicker
-        self.lights = self.pixelmatrix.get_lights(self.kwargs["light_selection"])
+            self.flicker = flicker
+        self.lights = self.pixelmatrix.get_lights(self.light_selection)
 
     def sync_send(self):
         """sync will be performed from prim devices to non prim devices"""
-        keys_to_export = ["light_selection"]
-        sync_kwargs = {key: self.kwargs[key] for key in keys_to_export}
-        self.settings.sync_dict = sync_kwargs
+        # only export light_selection
+        return dict(light_selection=self.light_selection)
 
     def sync_load(self, in_dict: dict):
-        # todo: not implemented
-        self.lights = self.pixelmatrix.get_lights(self.kwargs["light_selection"])
+        super().sync_load(in_dict=in_dict)
+        self.lights = self.pixelmatrix.get_lights(self.light_selection)
 
     def reset(self):
         self.matrix[:, :] = 0
         self.n_beats = -1
 
     def on_trigger(self) -> None:
-        if self.kwargs["light_selection"] == "random":
-            self.lights = self.pixelmatrix.get_lights(self.kwargs["light_selection"])
+        if self.light_selection == "random":
+            self.lights = self.pixelmatrix.get_lights(self.light_selection)
         # this pattern has to be reset for new meteor
         self.n_beats = -1
         if p(0.2):
@@ -117,7 +114,7 @@ class PatternMeteor(Pattern):
         # -> 1 | 1 | 0.5
         #    40% | 40 % | 20 %
         # n_beats * 1/2.5 + self.beat_progress / 2.5
-        if self.settings.beat_state == BeatStatePattern(loop_length=1):
+        if BeatStatePattern(loop_length=1).is_match(self.settings.beat_state):
             self.n_beats += 1
         self.pos = int((self.n_leds - self.width) * (self.n_beats + self.settings.beat_progress) / self.travel_time)
 
@@ -129,20 +126,21 @@ class PatternMeteor(Pattern):
         spawn_chance_np = np.fmax(1.3 - np.abs(self.pos - np.arange(self.n_leds)) / self.width, 0)
         for light_id in self.lights:
             matrix_view = matrix[:, light_id]
+
             # duplicate
             spawn_chance_np_new = spawn_chance_np.copy()
 
             # apply mirror_1
-            if self.kwargs["mirror"] == "out_to_in":
+            if self.mirror == "out_to_in":
                 spawn_chance_np_new[self.n_leds // 2 :] = np.flip(spawn_chance_np_new)[self.n_leds // 2 :]
 
-            if self.kwargs["mirror"] == "in_to_out":
+            if self.mirror == "in_to_out":
                 spawn_chance_np_new = np.flip(spawn_chance_np_new)
                 spawn_chance_np_new = np.roll(spawn_chance_np_new, -self.n_lights // 2)
                 spawn_chance_np_new[self.n_leds // 2 :] = np.flip(spawn_chance_np_new)[self.n_leds // 2 :]
 
             # apply flip
-            if self.kwargs["mirror"] == "through_magic":
+            if self.mirror == "through_magic":
                 if p(0.5):
                     spawn_chance_np_new = np.flip(spawn_chance_np_new)
 
@@ -155,7 +153,7 @@ class PatternMeteor(Pattern):
             matrix_view[:] = np.fmax(matrix_view, intensity)
 
             # apply mirror 2
-            if self.kwargs["mirror"] == "through":
+            if self.mirror == "through":
                 matrix_view[:] = np.flip(matrix_view)
 
         self.matrix = matrix
