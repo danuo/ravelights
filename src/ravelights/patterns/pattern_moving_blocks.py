5aa31d31231125dc2c96c344b950223eb0835c8d
Daniel <d.brummerloh@gmail.com>
2023-08-17 18:31:44 +0200

    PatternMovingBlocks: improve performance, fix crash, add roll effect

--- src/ravelights/patterns/pattern_moving_blocks.py
+++ src/ravelights/patterns/pattern_moving_blocks.py
@@ -37,32 +37,28 @@ class PatternMovingBlocks(Pattern):
         version = random.choice([0, 1, 2, 3])
         # version = 0
         if version == 0:
-            self.n_states = 50
             self.n_lengths = 10
             self.length_step_factor = 2
-            self.n_speed = 20
+            self.max_speed = 4
             self.n_items = 10
             self.brightness = 1.0
         if version == 1:
-            self.n_states = 50
             self.n_lengths = 8
             self.length_step_factor = 3
-            self.n_speed = 20
+            self.max_speed = 3
             self.n_items = 15
             self.brightness = 1.0
         if version == 2:
-            self.n_states = 50
             self.n_lengths = 10
             self.length_step_factor = 4
-            self.n_speed = 0  # no speed but reset constantly
+            self.max_speed = 0  # no speed but reset constantly
             self.n_items = 15
             self.brightness = 1.0
             # self.possible_triggers = ["0", "0,1", "0,2", "1,2,3,4"]
         if version == 3:
-            self.n_states = 50
             self.n_lengths = 10
             self.length_step_factor = 4
-            self.n_speed = 2
+            self.max_speed = 2
             self.n_items = 15
             self.brightness = 0.5
 
@@ -73,14 +69,15 @@ class PatternMovingBlocks(Pattern):
                 yield num
                 num = int(num + num / self.length_step_factor)
 
-        lengths = [next(sequence_func(4)) for _ in range(self.n_lengths)]
+        n_items = [next(sequence_func(4)) for _ in range(self.n_lengths)]
         self.prerendered_matrices: list[ArrayNx1] = []
-        for length in lengths:
+        for length in n_items:
             matrix = np.zeros(shape=(self.matrix_length))
             a = int(self.matrix_length / 2 - length / 2)
             b = int(self.matrix_length / 2 + length / 2)
             matrix[a:b] = 1.0
             self.prerendered_matrices.append(matrix)
+        self.reset()
 
     def reset(self):
         self.states: list[State] = []
@@ -88,21 +85,31 @@ class PatternMovingBlocks(Pattern):
             item_id = random.randrange(0, len(self.prerendered_matrices))
             bright = random.uniform(0, 1)
             pos = random.uniform(0, self.matrix_length)
-            speed = 0 if self.n_speed == 0 else random.uniform(-self.n_speed, self.n_speed)
+            speed = 0 if self.max_speed == 0 else random.uniform(-self.max_speed, self.max_speed)
             self.states.append(State(item_id, bright, pos, speed))
+        self.roll_speeds = [random.uniform(-5, 5) for _ in range(self.n_lights)]
+        self.rolls = [0.0] * self.n_lights
 
     def on_trigger(self):
         self.reset()
 
     def render(self, color: Color):
         matrix = self.get_float_matrix_2d_mono()
-        for light_id in range(self.n_lights):
-            matrix_view = matrix[:, light_id]
-            for state in self.states:
-                item_id, bright, pos, speed = state
-                rolled_view = np.roll(self.prerendered_matrices[item_id], shift=int(pos))
-                matrix_view[:] = matrix_view + self.brightness * bright * rolled_view[: self.pixelmatrix.n_leds]
-                state.pos = state.pos + speed
+        matrix = np.zeros((self.n_leds))
+        for state in self.states:
+            item_id, bright, pos, speed = state
+            rolled_view = np.roll(self.prerendered_matrices[item_id], shift=int(pos))
+            matrix[:] = matrix + self.brightness * bright * rolled_view[: self.pixelmatrix.n_leds]
+            state.pos = state.pos + speed
         matrix = self.pixelmatrix.clip_matrix_to_1(matrix)
+        matrix = np.repeat(matrix[..., None], repeats=self.n_lights, axis=-1)
+
+        rolls = True
+        if rolls:
+            for index in range(self.n_lights):
+                self.rolls[index] += self.roll_speeds[index]
+            for index in range(self.n_lights):
+                roll = int(round(self.rolls[index]))
+                matrix[:, index] = np.roll(matrix[:, index], axis=0, shift=roll)
         matrix_rgb = self.colorize_matrix(matrix, color=color)
         return matrix_rgb
