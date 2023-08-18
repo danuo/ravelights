83ab23218eaa842a5f45648844a75e16dc0299b8
Daniel <d.brummerloh@gmail.com>
2023-08-18 23:20:02 +0200

    PatternShadowSmall: make it possible to render multiple lights

--- src/ravelights/patterns/pattern_shadow_small.py
+++ src/ravelights/patterns/pattern_shadow_small.py
@@ -43,45 +43,39 @@ class PatternShadowSmall(Pattern):
 
     def render(self, color: Color) -> ArrayNx3:
         self.pos = self.pos + self.vel
-        if self.pos > self.n_leds + self.max_dist:
-            self.pos = -self.max_dist
+        matrix = self.get_float_matrix_2d_mono()
+        for light_index in range(self.n_lights):
+            pos = self.pos
+            if pos > self.n_leds + self.max_dist:
+                pos = -self.max_dist
 
-        matrix = self.get_float_matrix_rgb()
+            for index in range(self.grid_n - 1):
+                poleA = self.gutter[index]
+                poleB = self.gutter[index + 1]
+                middle = (poleB + poleA) / 2
+                dist = abs(pos - middle)
 
-        for index in range(self.grid_n - 1):
-            poleA = self.gutter[index]
-            poleB = self.gutter[index + 1]
-            middle = (poleB + poleA) / 2
-            dist = abs(self.pos - middle)
+                if pos < poleA:
+                    alpha = math.atan((poleA - pos) / self.dist_light)
+                    projection_A = poleA + math.tan(alpha) * (self.dist_screen + self.dist_gutter)
+                else:
+                    alpha = math.atan((poleA - pos) / (self.dist_light + self.dist_gutter))
+                    projection_A = poleA + math.tan(alpha) * self.dist_screen
+                projection_A = max(0, projection_A)
 
-            if self.pos < poleA:
-                alpha = math.atan((poleA - self.pos) / self.dist_light)
-                projection_A = poleA + math.tan(alpha) * (self.dist_screen + self.dist_gutter)
-            else:
-                alpha = math.atan((poleA - self.pos) / (self.dist_light + self.dist_gutter))
-                projection_A = poleA + math.tan(alpha) * self.dist_screen
-            projection_A = max(0, projection_A)
+                if pos < poleB:
+                    beta = math.atan((poleB - pos) / (self.dist_light + self.dist_gutter))
+                    projection_B = poleB + math.tan(beta) * self.dist_screen
+                else:
+                    beta = math.atan((poleB - pos) / (self.dist_light))
+                    projection_B = poleB + math.tan(beta) * (self.dist_screen + self.dist_gutter)
 
-            if self.pos < poleB:
-                beta = math.atan((poleB - self.pos) / (self.dist_light + self.dist_gutter))
-                projection_B = poleB + math.tan(beta) * self.dist_screen
-            else:
-                beta = math.atan((poleB - self.pos) / (self.dist_light))
-                projection_B = poleB + math.tan(beta) * (self.dist_screen + self.dist_gutter)
-
-            pax_int = int(round(projection_A))
-            pbx_int = int(round(projection_B))
-            # green:
-            if pbx_int > 0:
-                matrix[pax_int : pbx_int + 1, 0, 1] += 1 / (dist * 0.1)
-            # blue:
-            matrix[int(round(poleA)), 1, 2] += 0.2
-            matrix[int(round(poleB)), 1, 2] += 0.2
-            # red, x:
-            matrix[self.pos : self.pos + self.width, 2, 0] = 1.0
+                pax_int = int(round(projection_A))
+                pbx_int = int(round(projection_B))
+                # green:
+                if pbx_int > 0:
+                    matrix[pax_int : pbx_int + 1, light_index] += 1 / (dist * 0.1)
 
         matrix = np.fmin(1.0, matrix)
-        return matrix
-        # matrix_rgb = self.colorize_matrix(matrix, color=color)
-        # matrix[start:end, :] = 1
-        # return matrix_rgb
+        matrix_rgb = self.colorize_matrix(matrix, color=color)
+        return matrix_rgb
