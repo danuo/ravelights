92ffa86b706cd32c5ed02bb32068ce743f121b9b
niliha <75397148+niliha@users.noreply.github.com>
2023-08-26 17:29:51 +0200

    add dimer sideswipe

---
+++ src/ravelights/dimmers/dimmer_sideswipe.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from ravelights.core.colorhandler import Color
+from ravelights.core.custom_typing import ArrayMxKx3
+from ravelights.core.generator_super import Dimmer
+from ravelights.core.utils import p
+
+
+class DimmerSideswipe(Dimmer):
+    def init(self):
+        if self.version == 0:
+            self.frequency = 1
+        elif self.version == 1:
+            self.frequency = 2
+
+    def alternate(self):
+        self.flip = p(0.5)
+
+    def reset(self):
+        ...
+
+    def on_trigger(self):
+        ...
+
+    def render(self, in_matrix: ArrayMxKx3, color: Color) -> ArrayMxKx3:
+        progress = self.settings.bpmhandler.get_beat_progress_n(self.frequency)
+
+        mask = self.get_float_matrix_2d_mono()
+        n = int(progress * self.n_leds)
+        mask[n:, :] = 1
+        if self.flip:
+            mask = np.flip(mask, axis=0)
+
+        matrix = self.apply_mask(in_matrix=in_matrix, mask=mask)
+        return matrix
