847e0b63aea34d13fd2cfd5f89fcf00ff9d4be95
Daniel <d.brummerloh@gmail.com>
2023-10-28 21:32:52 +0200

    WIP test

--- manual_tests/t_get_random_names.py
+++ manual_tests/t_get_random_names.py
@@ -1,31 +1,24 @@
 from collections import Counter
 
 import matplotlib.pyplot as plt
-
-from main import create_devices
+from ravelights import DeviceDict, RaveLightsApp
 from ravelights.configs.components import Keywords as K
-from ravelights.configs.visualizer_configurations import device_configs
-from ravelights.core.device import Device
 from ravelights.core.generator_super import Pattern
-from ravelights.core.patternscheduler import PatternScheduler
-from ravelights.core.settings import Settings
 from ravelights.core.templateobjects import GenSelector
 
-settings = Settings(device_config=device_configs[0], bpm=80, fps=20)
-devices = create_devices(settings=settings)
-patternscheduler = PatternScheduler(settings=settings, devices=devices)
+app = RaveLightsApp(device_config=[DeviceDict(n_lights=10, n_leds=144)])
 
-names = []
+names: list[str] = []
 
 for _ in range(10000):
-    # sel = GenSelector(gen_type=Pattern, patternscheduler=patternscheduler)
-    sel = GenSelector(gen_type=Pattern, keywords=[K.STROBE], patternscheduler=patternscheduler)
-    names.append(sel.pattern_name)
+    gen_selector = GenSelector(root=app, gen_type=Pattern, keywords=[K.STROBE])
+    names.append(gen_selector.pattern_name)
 
 
 c1 = Counter(names)
 
 test = tuple(c1.items())
+counts: list[int]
 names, counts = zip(*tuple(c1.items()))
 
 plt.bar(names, counts)
