e6d110f43c62942dccd43e7da0bbb504856a3932
Daniel <d.brummerloh@gmail.com>
2023-10-29 23:00:01 +0100

    fix test

--- test/test_run_all_patterns.py
+++ test/test_run_all_patterns.py
@@ -1,12 +1,14 @@
+import logging
 import random
 
 from ravelights.core.bpmhandler import BeatStatePattern
 from ravelights.core.ravelights_app import RaveLightsApp
 
-FPS = 20
+logger = logging.getLogger(__name__)
 
 
 def test_all_patterns():
+    FPS = 20
     global_time = 0
 
     def get_global_time():
@@ -20,7 +22,8 @@ def test_all_patterns():
         random_frame_time = 1 / FPS + norm
         global_time += random_frame_time
 
-    app = RaveLightsApp(fps=FPS, visualizer=False, restapi=False)
+    # app = RaveLightsApp(fps=FPS, visualizer=False, restapi=False)
+    app = RaveLightsApp(run=False)
     app.settings.timehandler.get_current_time = get_global_time
     app.settings.timehandler.after = increase_globaltime
     app.patternscheduler.load_timeline_from_index(1)  # todo: load by name
@@ -32,7 +35,7 @@ def test_all_patterns():
     # get all generator names
     gen_names = []
     gen_types = []
-    for i, (key, lis) in enumerate(app.settings.meta["available_generators"].items()):
+    for i, (key, lis) in enumerate(app.metahandler.api_content["available_generators"].items()):
         if i == 4:
             break
         for dictionary in lis:
@@ -43,15 +46,15 @@ def test_all_patterns():
 
     done = False
     while not done:
-        if app.settings.beat_state == beat_matcher:
+        if beat_matcher.is_match(app.settings.beat_state):
             counter_generators += 1
             gen_type, gen_name = next(it, (None, None))
             if gen_type is None:
                 done = True
             else:
-                app.settings.set_generator(gen_type=gen_type, timeline_level=1, gen_name=gen_name)
+                app.settings.set_generator(gen_type=gen_type, timeline_level=1, gen_name=gen_name, renew_trigger=False)
 
         counter_frame += 1
         app.render_frame()
-    print(f"tested {counter_frame} frames")
-    print(f"tested {counter_generators} generators")
+    logger.info(f"tested {counter_frame} frames")
+    logger.info(f"tested {counter_generators} generators")
