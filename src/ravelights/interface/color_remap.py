6583f98a61317f53fda82497350bacedeff6571e
Daniel <d.brummerloh@gmail.com>
2023-11-05 18:40:06 +0100

    first draft for color remapping functionality

---
+++ src/ravelights/interface/color_remap.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from ravelights.core.custom_typing import ArrayFloat
+
+ColorMapping = Callable[["ArrayFloat"], "ArrayFloat"]
+
+
+def linear_color_mapping(in_matrix: "ArrayFloat") -> "ArrayFloat":
+    print("linear_color_mapping()")
+    return in_matrix
+
+
+def ws2815_color_mapping(in_matrix: "ArrayFloat") -> "ArrayFloat":
+    print("ws2815_color_mapping()")
+    return in_matrix
+
+
+class ColorProfiles(Enum):
+    LINEAR: ColorMapping = linear_color_mapping
+    WS2815: ColorMapping = ws2815_color_mapping
