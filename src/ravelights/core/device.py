9fd393c9f22921be9836b7c5d3fed9a9ba1523de
Daniel <d.brummerloh@gmail.com>
2023-11-05 18:42:03 +0100

    update prim stuff and add get_matrix functions to Device

--- src/ravelights/core/device.py
+++ src/ravelights/core/device.py
@@ -1,11 +1,13 @@
 import logging
 from typing import TYPE_CHECKING, Any
 
+from ravelights.core.custom_typing import ArrayFloat, ArrayUInt8
 from ravelights.core.instructionhandler import InstructionHandler
 from ravelights.core.pixelmatrix import PixelMatrix
 from ravelights.core.rendermodule import RenderModule
 from ravelights.core.settings import Settings
 from ravelights.core.timehandler import TimeHandler
+from ravelights.interface.color_remap import ColorProfiles
 
 if TYPE_CHECKING:
     from ravelights.core.ravelights_app import RaveLightsApp
@@ -15,15 +17,23 @@ logger = logging.getLogger(__name__)
 
 
 class Device:
-    def __init__(self, root: "RaveLightsApp", device_id: int, n_leds: int, n_lights: int, is_prim: bool = False):
+    def __init__(
+        self,
+        root: "RaveLightsApp",
+        device_id: int,
+        n_leds: int,
+        n_lights: int,
+        color_profile: ColorProfiles,
+    ):
         self.root = root
         self.device_id: int = device_id
         self.n_leds: int = n_leds
         self.n_lights: int = n_lights
-        self.is_prim: bool = is_prim
+        self.color_profile: ColorProfiles = color_profile
+        self.is_prim: bool = True if device_id == 0 else False
         self.settings: "Settings" = self.root.settings
         self.timehandler: "TimeHandler" = self.settings.timehandler
-        self.pixelmatrix = PixelMatrix(n_leds=n_leds, n_lights=n_lights, is_prim=is_prim)
+        self.pixelmatrix = PixelMatrix(n_leds=n_leds, n_lights=n_lights, is_prim=self.is_prim)
         self.rendermodule = RenderModule(root=root, device=self)
         self.instructionhandler = InstructionHandler(
             pixelmatrix=self.pixelmatrix,
@@ -39,6 +49,14 @@ class Device:
 
     def render(self):
         self.rendermodule.render()
+
+    def get_matrix_processed_float(self) -> ArrayFloat:
+        matrix_float = self.pixelmatrix.get_matrix_float()
+        brightness = min(self.settings.global_brightness, self.device_brightness)
+        return self.color_profile(matrix_float * brightness)
+
+    def get_matrix_int(self) -> ArrayUInt8:
+        return self.pixelmatrix.get_matrix_int()
 
     def get_device_objects(self) -> dict[str, Settings | TimeHandler | PixelMatrix]:
         return dict(settings=self.settings, pixelmatrix=self.pixelmatrix)
