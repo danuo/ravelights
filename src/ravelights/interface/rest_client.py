7d0c084655d718d990e5bc9d8a0a2272d4626f04
Daniel <d.brummerloh@gmail.com>
2023-12-15 13:37:16 +0100

    python stuff

--- src/ravelights/interface/rest_client.py
+++ src/ravelights/interface/rest_client.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from typing import Optional
 
 import requests
 
@@ -7,16 +8,16 @@ logger = logging.getLogger(__name__)
 
 
 class RestClient:
-    def __init__(self, ip_address: str, port: int = 80):
-        self._OUTPUT_CONFIG_LENGTH = 4
-        self._REQUEST_TIMEOUT_SECONDS = 3
+    OUTPUT_CONFIG_LENGTH: int = 4
+    REQUEST_TIMEOUT_SECONDS: float = 3.0
 
+    def __init__(self, ip_address: str, port: int = 80):
         base_url = f"http://{ip_address}:{port}/api"
         self._config_url = f"{base_url}/config"
 
-    def get_output_config(self) -> list[int] | None:
+    def get_output_config(self) -> Optional[list[int]]:
         try:
-            response = requests.get(self._config_url, timeout=self._REQUEST_TIMEOUT_SECONDS)
+            response = requests.get(self._config_url, timeout=self.REQUEST_TIMEOUT_SECONDS)
             response.raise_for_status()
         except requests.RequestException as e:
             logger.error(f"Error getting output config: {e}")
@@ -29,11 +30,15 @@ class RestClient:
             return None
 
         if not (
-            isinstance(output_config, list)
-            and len(output_config) == self._OUTPUT_CONFIG_LENGTH
-            and all(isinstance(entry, int) for entry in output_config)
+            all(
+                [
+                    isinstance(output_config, list),
+                    len(output_config) == self.OUTPUT_CONFIG_LENGTH,
+                    all(isinstance(entry, int) for entry in output_config),
+                ]
+            )
         ):
-            logger.error(f"Output config is not a list of 4 ints: {output_config}")
+            logger.error(f"Output config is not a list of {self.OUTPUT_CONFIG_LENGTH} ints: {output_config}")
             return None
 
         return output_config
@@ -44,7 +49,7 @@ class RestClient:
         headers = {"Content-Type": "application/json"}
         try:
             response = requests.post(
-                self._config_url, data=json.dumps(output_config), headers=headers, timeout=self._REQUEST_TIMEOUT_SECONDS
+                self._config_url, data=json.dumps(output_config), headers=headers, timeout=self.REQUEST_TIMEOUT_SECONDS
             )
             response.raise_for_status()
         except requests.RequestException as e:
