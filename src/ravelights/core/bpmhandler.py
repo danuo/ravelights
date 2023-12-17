a77a78c732ed0e7a5dfa3fcd3ba8edefa4a53d56
Daniel <d.brummerloh@gmail.com>
2023-12-17 23:41:49 +0100

    delete file

--- src/ravelights/core/bpmhandler.py
+++
@@ -1,8 +0,0 @@
-# class BPMhandler:
-#     def __init__(self, settings: "Settings", timehandler: "TimeHandler"):
-#         self.settings = settings
-#         self.timehandler = timehandler
-#         self.bpm_sync()
-#         self.n_quarters_long_memory = 0
-#         self.time_0_cache: float = self.timehandler.time_0
-#         self.beat_state_cache: BeatState = BeatState(self.settings)
