63035fbfdc183dcf8f0891533e08bcff6726ae09
niliha <75397148+niliha@users.noreply.github.com>
2023-12-17 20:21:20 +0100

    Continuously discover pixeldrivers using mDNS

--- src/ravelights/__init__.py
+++ src/ravelights/__init__.py
@@ -4,8 +4,6 @@ from ravelights.core.ravelights_app import RaveLightsApp
 from ravelights.devtools.profiler import Profiler
 from ravelights.interface.artnet.artnet_udp_transmitter import ArtnetUdpTransmitter
 from ravelights.interface.color_remap import ColorProfiles
-from ravelights.interface.discovery import discover_pixeldrivers
-from ravelights.interface.rest_client import RestClient
 
 try:
     from ravelights.interface.artnet.artnet_serial_transmitter import ArtnetSerialTransmitter
@@ -20,7 +18,5 @@ __all__ = [
     "TransmitterConfig",
     "ArtnetUdpTransmitter",
     "ArtnetSerialTransmitter",
-    "discover_pixeldrivers",
-    "RestClient",
     "ColorProfiles",
 ]
