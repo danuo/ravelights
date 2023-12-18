26a451ef236d94a8fc0d4072ecca120e0a956d3b
Daniel <d.brummerloh@gmail.com>
2023-12-19 00:55:24 +0100

    wip fix edge detect, still not working properly

--- src/ravelights/vfilters/vfilter_edgedetect.py
+++ src/ravelights/vfilters/vfilter_edgedetect.py
@@ -1,4 +1,5 @@
 import numpy as np
+from icecream import ic
 
 from ravelights.core.colorhandler import Color
 from ravelights.core.custom_typing import ArrayFloat
@@ -28,25 +29,40 @@ class VfilterEdgedetect(Vfilter):
 
     def render(self, in_matrix: ArrayFloat, colors: list[Color]) -> ArrayFloat:
         # bw
-        color = np.asanyarray(colors[0])
+        # color = np.asanyarray(colors[0])
         bw_matrix_mono = self.bw_matrix(in_matrix)
 
         # get color
         # todo: cleanup
         rgb_sum = np.sum(in_matrix, axis=-1).reshape((-1))
         max_id = np.argmax(rgb_sum)
-        if rgb_sum[max_id] > 0:
-            color = in_matrix.reshape(-1, 3, order="F")[max_id]
-            color = color / np.max(color)
+        # ! still bugged!
+        if rgb_sum[max_id] == 0:
+            return in_matrix
+        else:
+            color = in_matrix.reshape(-1, 3, order="F")
+            # print(in_matrix.reshape(-1, 3, order="F")[:10])
+            ic(np.max(color).shape)
+            divisor = np.max(color, axis=-1)
+            non_zero_divisor = np.fmin(0.0001, divisor)
+            color = color / non_zero_divisor[..., None]
+            # ! bug. should divide through np.max of that line, not np.max from total
             color = np.fmin(1, color)
+        # ic(color.shape)
 
         # find edge
         roll = np.roll(bw_matrix_mono, shift=1, axis=0)
         roll[0] = 0  # does this work?
         diff = np.abs(bw_matrix_mono - roll)
 
+        ic(color.shape)
+        ic(color[10:])
+
         if self.version == 0:
-            return self.colorize_matrix(diff, color)
+            ic(diff.shape)
+            ic(color.shape)
+            # return self.colorize_matrix(diff, color)
+            return diff[..., None] * color.reshape((self.n_leds, self.n_lights, 1))
 
         # expand
         self.expansion_matrix[..., 0] = diff
@@ -59,4 +75,7 @@ class VfilterEdgedetect(Vfilter):
             roll[-1] = 0
             self.expansion_matrix[..., 2 * i + 2] = roll  # 2, 4, 6
 
-        return self.colorize_matrix(np.max(self.expansion_matrix, axis=-1), color)
+        bw_out = np.max(self.expansion_matrix, axis=-1)
+
+        ic(bw_out.shape)
+        return bw_out[..., None] * color.reshape((self.n_leds, self.n_lights, 3))
