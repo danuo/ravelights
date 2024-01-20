3ad64826e24c550f497f7a92c5721dcea80a5195
danuo <anon@anon.com>
2024-01-20 22:42:06 +0100

    refactor audio data

--- src/ravelights/audio/audio_analyzer_process.py
+++ src/ravelights/audio/audio_analyzer_process.py
@@ -1,15 +1,61 @@
 import random
 import time
 from multiprocessing.connection import _ConnectionBase
+from typing import TypedDict
+
+
+class AudioData(TypedDict):
+    level: float
+    level_low: float
+    level_mid: float
+    level_high: float
+
+    hits: float
+    hits_low: float
+    hits_mid: float
+    hits_high: float
+
+    presence: float
+    presence_low: float
+    presence_mid: float
+    presence_high: float
+
+    FadeInOut: float
+    is_beat: bool
+
+
+class AudioAnalyzer:
+    audio_data = AudioData(
+        level=0,
+        level_low=0,
+        level_mid=0,
+        level_high=0,
+        hits=0,
+        hits_low=0,
+        hits_mid=0,
+        hits_high=0,
+        presence=0,
+        presence_low=0,
+        presence_mid=0,
+        presence_high=0,
+        FadeInOut=0,
+        is_beat=False,
+    )
+
+    def process_audio(self, audio):
+        pass
 
 
 def audio_analyzer_process(connection: _ConnectionBase):
+    audio_analyzer = AudioAnalyzer()
+
     while True:
         # send random data at high rate
-        random_data = {
+        audio_data = {
             "random_float": random.random(),
             "random_bool": random.random() > 0.5,
         }
-        print(f"sending {random_data}")
-        connection.send(random_data)
+
+        audio_analyzer.process_audio(audio_data)
+        connection.send(audio_analyzer.audio_data)
         time.sleep(1 / 100)
--- src/ravelights/audio/audio_data.py
+++ src/ravelights/audio/audio_data.py
@@ -1,59 +1,32 @@
 from multiprocessing.connection import _ConnectionBase
-from typing import Optional, TypedDict
+from typing import TYPE_CHECKING, Optional
 
+from ravelights.audio.audio_analyzer_process import AudioData
 
-class AudioData(TypedDict):
-    level: float
-    level_low: float
-    level_mid: float
-    level_high: float
-
-    hits: float
-    hits_low: float
-    hits_mid: float
-    hits_high: float
-
-    presence: float
-    presence_low: float
-    presence_mid: float
-    presence_high: float
-
-    is_beat: bool
-    FadeInOut: float
+if TYPE_CHECKING:
+    from ravelights import RaveLightsApp
 
 
 class AudioDataProvider:
-    level: float
-    level_low: float
-    level_mid: float
-    level_high: float
-
-    hits: float
-    hits_low: float
-    hits_mid: float
-    hits_high: float
-
-    presence: float
-    presence_low: float
-    presence_mid: float
-    presence_high: float
-
-    is_beat: bool
-    FadeInOut: float
-
+    audio_data: Optional[AudioData] = None
     connection: Optional[_ConnectionBase] = None
 
-    def set_connection(self, connection: _ConnectionBase = None):
+    def __init__(self, root: "RaveLightsApp"):
+        self.root = root
+
+    def set_connection(self, connection: _ConnectionBase) -> None:
         self.connection = connection
 
-    def collect_audio_data(self):
+    def collect_audio_data(self) -> None:
         if self.connection is None:
             return
 
         # all data in the pipe until pipe is empty
-        self.data = []
+        self.data: list[AudioData] = []
         while self.connection.poll():
             self.data.append(self.connection.recv())
+
+        self.audio_data = self.data[-1]
 
         # process data
         print(f"receiving at fps rate {self.data}")
