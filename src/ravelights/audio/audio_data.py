8accacfb9b5364572f4b07c28b8a33ae2546eb25
danuo <anon@anon.com>
2024-01-20 01:41:00 +0100

    define audio api

--- src/ravelights/audio/audio_data.py
+++ src/ravelights/audio/audio_data.py
@@ -1,8 +1,46 @@
 from multiprocessing.connection import _ConnectionBase
-from typing import Optional
+from typing import Optional, TypedDict
+
+
+class AudioData(TypedDict):
+    level: float
+    level_low: float
+    level_mid: float
+    level_high: float
+
+    hits: float
+    hits_low: float
+    hits_mid: float
+    hits_high: float
+
+    presence: float
+    presence_low: float
+    presence_mid: float
+    presence_high: float
+
+    is_beat: bool
+    FadeInOut: float
 
 
 class AudioDataProvider:
+    level: float
+    level_low: float
+    level_mid: float
+    level_high: float
+
+    hits: float
+    hits_low: float
+    hits_mid: float
+    hits_high: float
+
+    presence: float
+    presence_low: float
+    presence_mid: float
+    presence_high: float
+
+    is_beat: bool
+    FadeInOut: float
+
     connection: Optional[_ConnectionBase] = None
 
     def set_connection(self, connection: _ConnectionBase = None):
