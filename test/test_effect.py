4de8a67518e5fcf61b27de02b5c37ad6755a59fa
danuo <anon@anon.com>
2024-02-18 21:02:37 +0100

    move limit test

--- test/test_effect.py
+++ test/test_effect.py
@@ -16,3 +16,28 @@ def test_get_frames_pattern_binary():
     frames_pattern = FramesPattern(3, (0, 2))
     result = get_frames_pattern_binary(frames_pattern, multi=2)
     assert result == [True, True, False, False, True, True]
+
+
+def test_effect_frames_limit(app_time_patched_2):
+    app = app_time_patched_2
+    app.settings.enable_autopilot = False
+
+    # get 3 effect names
+    effect_names = app.meta_handler.api_content["available_generators"]["effect"][:3]
+
+    # add effects with 15 frames duration
+    for effect_name in effect_names:
+        app.effect_handler.clear_qeueues()
+        assert len(app.effect_handler.effect_queue) == 0
+        app.effect_handler.load_effect(effect_name=effect_name, mode="frames", limit_frames=15)
+        assert len(app.effect_handler.effect_queue) == 1
+
+        for _ in range(10):
+            app.render_frame()
+
+        assert len(app.effect_handler.effect_queue) == 1
+
+        for _ in range(10):
+            app.render_frame()
+
+        assert len(app.effect_handler.effect_queue) == 0
--- test/test_run_all_effects.py
+++ test/test_run_all_effects.py
@@ -3,15 +3,11 @@ from ravelights import RaveLightsApp
 LIMIT = 50
 
 
-def test_all_effects_2_inf(app_time_patched_2):
-    run_all_effects_inf(app_time_patched_2)
+def test_all_effects_2(app_time_patched_2):
+    run_all_effects(app_time_patched_2)
 
 
-def test_all_effects_2_limited(app_time_patched_2):
-    run_all_effects_limited(app_time_patched_2)
-
-
-def run_all_effects_inf(app: RaveLightsApp):
+def run_all_effects(app: RaveLightsApp):
     app.settings.enable_autopilot = False
 
     effect_names = []
@@ -28,29 +24,3 @@ def run_all_effects_inf(app: RaveLightsApp):
 
         for _ in range(LIMIT):
             app.render_frame()
-
-
-def run_all_effects_limited(app: RaveLightsApp):
-    app.settings.enable_autopilot = False
-
-    effect_names = []
-
-    for dictionary in app.meta_handler.api_content["available_generators"]["effect"]:
-        effect_names.append(dictionary["generator_name"])
-
-    # add effects with 15 frames duration
-    for effect_name in effect_names:
-        app.effect_handler.clear_qeueues()
-        assert len(app.effect_handler.effect_queue) == 0
-        app.effect_handler.load_effect(effect_name=effect_name, mode="frames", limit_frames=15)
-        assert len(app.effect_handler.effect_queue) == 1
-
-        for _ in range(10):
-            app.render_frame()
-
-        assert len(app.effect_handler.effect_queue) == 1
-
-        for _ in range(10):
-            app.render_frame()
-
-        assert len(app.effect_handler.effect_queue) == 0
