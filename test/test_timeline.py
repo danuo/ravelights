5dc35411388b8e15f141d850d2a863dae512d836
danuo <anon@anon.com>
2024-03-21 23:46:52 +0100

    add more timeline tests

--- test/test_timeline.py
+++ test/test_timeline.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+import numpy as np
 from ravelights import RaveLightsApp
 from ravelights.core.custom_typing import Timeline, TimelineMeta
 from ravelights.core.generator_super import (
@@ -27,17 +28,75 @@ def test_timeline(app_time_patched_1: RaveLightsApp):
 
     app = app_time_patched_1
     app.settings.enable_autopilot = False
+    app.settings.global_manual_timeline_level = None
 
     for selector in debug_timeline["selectors"]:
         selector.set_root(app)
 
     app.pattern_scheduler._load_timeline(debug_timeline)
 
-    for _ in range(1_000):
+    N_FRAMES = 1_000
+
+    for _ in range(N_FRAMES):
         app.render_frame()
         effective_timeline_level = app.pattern_scheduler.get_effective_timeline_level(0)
         counter.update((effective_timeline_level,))
 
-    assert counter[1] > 100
-    assert counter[2] > 100
-    assert counter[3] > 100
+    assert counter[1] > 0
+    assert counter[2] > 0
+    assert counter[3] > 0
+
+    np.testing.assert_allclose(counter[1], N_FRAMES * 0.25, rtol=0.1), counter
+    np.testing.assert_allclose(counter[2], N_FRAMES * 0.25, rtol=0.1), counter
+    np.testing.assert_allclose(counter[3], N_FRAMES * 0.5, rtol=0.1), counter
+
+
+def test_manual_timeline_level(app_time_patched_1: RaveLightsApp):
+    counter: Counter[int] = Counter()
+
+    app = app_time_patched_1
+
+    app.settings.enable_autopilot = False
+    app.settings.global_manual_timeline_level = 2
+
+    for selector in debug_timeline["selectors"]:
+        selector.set_root(app)
+
+    app.pattern_scheduler._load_timeline(debug_timeline)
+
+    N_FRAMES = 1_000
+
+    for _ in range(N_FRAMES):
+        app.render_frame()
+        effective_timeline_level = app.pattern_scheduler.get_effective_timeline_level(0)
+        counter.update((effective_timeline_level,))
+
+    assert counter[1] == 0
+    assert counter[2] > 0
+    assert counter[3] == 0
+
+
+def test_device_manual_timeline_level(app_time_patched_1: RaveLightsApp):
+    counter: Counter[int] = Counter()
+
+    app = app_time_patched_1
+
+    app.settings.enable_autopilot = False
+    app.settings.global_manual_timeline_level = 2
+    app.devices[0].device_settings.device_manual_timeline_level = 3
+
+    for selector in debug_timeline["selectors"]:
+        selector.set_root(app)
+
+    app.pattern_scheduler._load_timeline(debug_timeline)
+
+    N_FRAMES = 1_000
+
+    for _ in range(N_FRAMES):
+        app.render_frame()
+        effective_timeline_level = app.pattern_scheduler.get_effective_timeline_level(0)
+        counter.update((effective_timeline_level,))
+
+    assert counter[1] == 0
+    assert counter[2] == 0
+    assert counter[3] > 0
